f82e29013e4dc420bffbcf75e7d310c279ad4277
Vladimir Antonov <v.antonov@ndmsystems.com>
2016-03-18 20:16:34 +0300

    dist.clean plugin fix

--- samples/plugins/dist.js
+++ samples/plugins/dist.js
@@ -6,7 +6,10 @@ var fs = require('nano-fs'),
 module.exports = {
 
 clean: function (log, data) {
-	return fs.empty(data.dist_folder || (data.opts && data.opts.dist_folder));
+	var dist_folder = data.dist_folder || (data.opts && data.opts.dist_folder);
+	return fs.mkpath(dist_folder).then(function () {
+		return fs.empty(dist_folder);
+	});
 }
 
 };
